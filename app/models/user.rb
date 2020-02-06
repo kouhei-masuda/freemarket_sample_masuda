0745145859ce583fa2ba7ce76333f0b20477fd94
kouhei-masuda <kohei.masuda@di-v.co.jp>
2020-02-06 21:30:46 +0900

    edit-user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :
+  
+  has_one :address, dependent: :destroy
+
+  validates :nickname, :birthday, :earnings, :points, presence: true
+  validates :first_name_reading, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
+  validates :last_name_reading, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'
    }
+
   devise_scope :user do
     get "users/select_registration", to: 'users/registration#select', as: :select_registration
   end
 
+ 
+ 
  
 end
