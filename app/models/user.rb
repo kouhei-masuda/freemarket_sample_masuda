eb73196d3ee101966e7664e9b266c5dd1685fc89
kouhei-masuda <kohei.masuda@di-v.co.jp>
2020-02-06 21:33:38 +0900

    edit-user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :
+         :recoverable, :rememberable
   
   has_one :address, dependent: :destroy
-
   validates :nickname, :birthday, :earnings, :points, presence: true
   validates :first_name_reading, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
   validates :last_name_reading, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
