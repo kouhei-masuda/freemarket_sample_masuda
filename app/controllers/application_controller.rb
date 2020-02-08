01885e05d21ed9ec857ba89d1743f7a32fed24dc
kouhei-masuda <kohei.masuda@di-v.co.jp>
2020-02-08 20:03:48 +0900

    update-routes.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   private 
 
-  def
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :avatar, :introduction, :first_name, :first_name_reading, :last_name, :last_name_reading, :birthday, :earnings, :points, address_attributes: [:phone_number, :postal_code, :prefecture, :city, :house_number, :building_name]])
-  end 
+  # def
+    # devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :avatar, :introduction, :first_name, :first_name_reading, :last_name, :last_name_reading, :birthday, :earnings, :points, address_attributes: [:phone_number, :postal_code, :prefecture, :city, :house_number, :building_name]])
+  # end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
    }
 
   devise_scope :user do
-    get "users/select_registration", to: 'users/registration#select', as: :select_registration
+    get "users/select_registration", to: 'users/registrations#select', as: :select_registration
+    get "users/confirm_phone", to: 'users/registrations#confirm_phone', as: :confirm_phone
   end
 
  
