a22c5c11f6c35ed42b3a46a7ecfa4c9c50f89750
kouhei-masuda <kohei.masuda@di-v.co.jp>
2020-02-10 14:44:12 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get "users/select_registration", to: 'users/registrations#select', as: :select_registration
     get "users/confirm_phone", to: 'users/registrations#confirm_phone', as: :confirm_phone
+    get "users/new_address", to: 'users/registrations#new_address', as: :new_regist_address
   end
 
  
