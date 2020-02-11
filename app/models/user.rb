b8f27979318f378d23d75927b8065d9ddfa37013
kouhei-masuda <kohei.masuda@di-v.co.jp>
2020-02-11 23:32:51 +0900

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable
   
-  accepts_nested_attributes_for :address
-  
   has_one :address, dependent: :destroy
   validates :nickname, :birthday, :earnings, :points, presence: true
   validates :first_name_reading, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
   validates :last_name_reading, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
+  accepts_nested_attributes_for :address
 end
